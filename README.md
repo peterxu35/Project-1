fe0dc4f30b8f1a97082e953c33ff648f98022064
peterxu35 <peter.px.xu@gmail.com>
2022-05-23 09:07:24 -0400

    Pseudocode

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Project-1
+# Project-1 Game Name
+
+Premise: Play a game of No Limit Texas Hold em. Bet money and 
+beat your opponents hand to get their money and win!
+
+User Story
+As a user I want to
+. Decide whether or not to play given 2 cards
+. Bet money or pass or fold 
+. See how much time left in a round
+. See how much money I and my opponent have
+. Shuffle deck after each round
+. Receive a win message when I have all the money
+. Receive a lose message when I have no money
+Technologies Used
+HTML
+CSS
+JavaScript (logic and DOM manipulation)
+Canvas (Optional)
+Wireframes:
+what game will look like
---
+++ app.js
@@ -0,0 +1,25 @@
+// // Variables
+// make array for deck
+// make arrays for players for their 2 cards 
+// make array for the 5 community cards
+// a variable that keeps track of a poker hand's strength
+// variables that keep track of player, opponent, and betting money
+// have an ante: the minimum amount you need to put in to win
+
+// //Constants
+// somehow make a tier list of poker hands
+// probably set each poker hand to equal a certain score
+
+
+
+
+// //Functions
+// one where it will take a card from the deck array and put it into another array
+// first three cards for community come out at the same time, then the next two 1 by 1 
+// compare the strength of poker hands to see who wins
+// end the game when a player has no money
+// increase the ante gradually if game takes too long
+
+
+// //other
+// have a bar that you can interact with that manipulates how much money youre betting
